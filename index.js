0ceb0ea3c2002c5f4e939e6cb0beb58059541c33
Sridharsasi <49144065+Sridharsasi@users.noreply.github.com>
2023-07-05 14:50:00 -0400

    updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+var cors = require('cors')
 const app = express();
 const mongoose = require("mongoose");
 const path = require('path');
@@ -11,11 +12,11 @@ mongoose.connect(process.env.dbURL)
   .catch(error => console.log(error));
 
 app.use(express.json());
-
+app.use(cors({origin:'*'}))
 app.use(express.static(__dirname + "/client/build"));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/client/build', 'index.html')));
+app.get('/', (_req, res) => res.sendFile(path.join(__dirname, '/client/build', 'index.html')));
 
-app.use(function(req, res, next) {
+app.use(function(_req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin,X-Requested-Width,Content-Type,Accept,Authorization");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
